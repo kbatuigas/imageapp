741e05f009a8f78e34e4fdc761604d1cf1221cdd
kbatuigas <36839689+kbatuigas@users.noreply.github.com>
2020-11-16 19:25:47 -0500

    Some cleanup

--- image_process/views.py
+++ image_process/views.py
@@ -19,10 +19,11 @@ def upload_file(request):
     if request.method == 'POST':
         form = UploadImageForm(request.POST, request.FILES)
         if form.is_valid():
-            # file is saved
-
+            # Don't save image to DB just yet
             uploaded_img = form.save(commit=False)
+            # Read bytes into BinaryField
             uploaded_img.image_data = form.cleaned_data['image'].file.read()
+            # Now we can save to DB
             uploaded_img.save()
             return redirect('/')    # Why didn't 'index' work?
     else:
@@ -42,31 +43,21 @@ class ImageListView(generic.ListView):
     model = ImageFile
     template_name = 'image_process/browse.html'
     context_object_name = 'images_all'
-    #print('Hello')
-    #queryset = ImageFile.objects.filter(image_data__isnull=False) #.values('image_data')
+
 
     def get_context_data(self, *args, **kwargs):
         context = super(ImageListView, self).get_context_data(*args, **kwargs)
         queryset = ImageFile.objects.filter(image_data__isnull=False)
         results = []
         for image_row in queryset:
-            right_image = base64.b64encode(image_row.image_data).decode('utf-8')
-            results.append(right_image)
+            enc_image = base64.b64encode(image_row.image_data).decode('utf-8')
+            results.append(enc_image)
 
         context['results'] = results
         return context
 
-    def get_images(self):
-        pass
 
 
-
-    #def get_context_data(self, *args, **kwargs):
-    #   context = super(ImageListView, self).get_context_data(*args, **kwargs)
-
-    #    context['test_set'] = img_set
-
-    #    return context
 
 
 
